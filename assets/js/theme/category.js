e8f4925e5d873b9a4c07cdde860cc74fec7006d2
Adam Beatty <68173919+atBeatty@users.noreply.github.com>
2022-01-04 19:18:33 -0800

    update category.js
    working on setting up a cart to id and ready to receive all product goods

--- assets/js/theme/category.js
+++ assets/js/theme/category.js
@@ -1,27 +1,27 @@
-import { hooks, api } from "@bigcommerce/stencil-utils";
-import CatalogPage from "./catalog";
-import compareProducts from "./global/compare-products";
-import FacetedSearch from "./common/faceted-search";
-import { createTranslationDictionary } from "../theme/common/utils/translations-utils";
+import { hooks, api } from "@bigcommerce/stencil-utils"
+import CatalogPage from "./catalog"
+import compareProducts from "./global/compare-products"
+import FacetedSearch from "./common/faceted-search"
+import { createTranslationDictionary } from "../theme/common/utils/translations-utils"
 
 export default class Category extends CatalogPage {
   constructor(context) {
-    super(context);
-    this.validationDictionary = createTranslationDictionary(context);
+    super(context)
+    this.validationDictionary = createTranslationDictionary(context)
   }
 
   setLiveRegionAttributes($element, roleType, ariaLiveStatus) {
     $element.attr({
       role: roleType,
       "aria-live": ariaLiveStatus,
-    });
+    })
   }
 
   makeShopByPriceFilterAccessible() {
-    if (!$("[data-shop-by-price]").length) return;
+    if (!$("[data-shop-by-price]").length) return
 
     if ($(".navList-action").hasClass("is-active")) {
-      $("a.navList-action.is-active").focus();
+      $("a.navList-action.is-active").focus()
     }
 
     $("a.navList-action").on("click", () =>
@@ -30,16 +30,16 @@ export default class Category extends CatalogPage {
         "status",
         "assertive"
       )
-    );
+    )
   }
 
   onReady() {
-    this.addAlternateImage();
-    this.addAllToCart();
+    this.addAlternateImage()
+    this.addAllToCart()
     // const imageHover = $("div.image-hover")[0];
     // imageHover.addEventListener("mouseover", this.onProductListingHover);
     // console.log(imageHover)
-    this.arrangeFocusOnSortBy();
+    this.arrangeFocusOnSortBy()
 
     $('[data-button-type="add-cart"]').on("click", (e) =>
       this.setLiveRegionAttributes(
@@ -47,30 +47,30 @@ export default class Category extends CatalogPage {
         "status",
         "polite"
       )
-    );
+    )
 
-    this.makeShopByPriceFilterAccessible();
+    this.makeShopByPriceFilterAccessible()
 
-    compareProducts(this.context);
+    compareProducts(this.context)
 
     if ($("#facetedSearch").length > 0) {
-      this.initFacetedSearch();
+      this.initFacetedSearch()
     } else {
-      this.onSortBySubmit = this.onSortBySubmit.bind(this);
-      hooks.on("sortBy-submitted", this.onSortBySubmit);
+      this.onSortBySubmit = this.onSortBySubmit.bind(this)
+      hooks.on("sortBy-submitted", this.onSortBySubmit)
     }
 
     $("a.reset-btn").on("click", () =>
       this.setLiveRegionsAttributes($("span.reset-message"), "status", "polite")
-    );
+    )
 
-    this.ariaNotifyNoProducts();
+    this.ariaNotifyNoProducts()
   }
 
   ariaNotifyNoProducts() {
-    const $noProductsMessage = $("[data-no-products-notification]");
+    const $noProductsMessage = $("[data-no-products-notification]")
     if ($noProductsMessage.length) {
-      $noProductsMessage.focus();
+      $noProductsMessage.focus()
     }
   }
 
@@ -81,10 +81,10 @@ export default class Category extends CatalogPage {
       price_min_not_entered: minPriceNotEntered,
       price_max_not_entered: maxPriceNotEntered,
       price_invalid_value: onInvalidPrice,
-    } = this.validationDictionary;
-    const $productListingContainer = $("#product-listing-container");
-    const $facetedSearchContainer = $("#faceted-search-container");
-    const productsPerPage = this.context.categoryProductsPerPage;
+    } = this.validationDictionary
+    const $productListingContainer = $("#product-listing-container")
+    const $facetedSearchContainer = $("#faceted-search-container")
+    const productsPerPage = this.context.categoryProductsPerPage
     const requestOptions = {
       config: {
         category: {
@@ -99,22 +99,22 @@ export default class Category extends CatalogPage {
         sidebar: "category/sidebar",
       },
       showMore: "category/show-more",
-    };
+    }
 
     this.facetedSearch = new FacetedSearch(
       requestOptions,
       (content) => {
-        $productListingContainer.html(content.productListing);
-        $facetedSearchContainer.html(content.sidebar);
+        $productListingContainer.html(content.productListing)
+        $facetedSearchContainer.html(content.sidebar)
 
-        $("body").triggerHandler("compareReset");
+        $("body").triggerHandler("compareReset")
 
         $("html, body").animate(
           {
             scrollTop: 0,
           },
           100
-        );
+        )
       },
       {
         validationErrorMessages: {
@@ -125,7 +125,18 @@ export default class Category extends CatalogPage {
           onInvalidPrice,
         },
       }
-    );
+    )
+  }
+
+  addCartItem(url, cartId, cartItems) {
+    return fetch(url + cartId + "/items", {
+      method: "POST",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(cartItems),
+    }).then((response) => response.json())
   }
 
   createCart(url, cartItems) {
@@ -136,63 +147,48 @@ export default class Category extends CatalogPage {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(cartItems),
-    }).then((response) => response.json());
+    }).then((response) => response.json())
   }
 
+  getCart(url) {
+    return fetch(url, {
+      method: "GET",
+      credentials: "same-origin",
+    }).then((response) => response.json())
+  }
   addAllToCart() {
-    this.createCart(`/api/storefront/carts`, {
-      lineItems: [
-        {
-          quantity: 1,
-          productId: 86,
-        },
-        {
-          quantity: 1,
-          productId: 88,
-        },
-      ],
+    const $button = $("#add-category-products")
+    const cart = () => {
+      this.getCart(
+        "/api/storefront/carts?include=lineItems.digitalItems.options,lineItems.physicalItems.options"
+      )
+        .then((data) => console.log(JSON.stringify(data)))
+        .catch((error) => console.error(error))
+    }
+    $button.on("click", (e) => {
+      cart()
     })
-      .then((data) => console.log(JSON.stringify(data)))
-      .catch((error) => console.error(error));
-    const addAllBtn = $("button#add-category-products");
-    addAllBtn.on("click", (e) => {
-      console.log(e);
-      api.cart.itemAdd({ action: "ADD", product_id: 123 }, (err, response) => {
-        console.log(response);
-      });
-    });
-    // api.cart.itemAdd(new FormData(form), (err, response) => {
-    //   const errorMessage = err || response.data.error;
-    //   // Guard statement
-    //   if (errorMessage) {
-    //     // Strip the HTML from the error message
-    //     const tmp = document.createElement("DIV");
-    //     tmp.innerHTML = errorMessage;
-
-    //     return alert(tmp.textContent || tmp.innerText);
-    //   }
-    // });
   }
 
   addAlternateImage() {
     // const cards = $('.card')
-    const cards = document.querySelectorAll(".card");
-    const cardArray = Array.from(cards);
+    const cards = document.querySelectorAll(".card")
+    const cardArray = Array.from(cards)
 
     cardArray.forEach((card) => {
-      const figCaption = card.querySelector("figcaption");
-      const firstImg = card.querySelector(".card-img-container img");
+      const figCaption = card.querySelector("figcaption")
+      const firstImg = card.querySelector(".card-img-container img")
       //   const secondImg = firstImg.nextElementSibling;
       card.addEventListener("mouseenter", (e) => {
         // secondImg.style.opacity = 0;
-        firstImg.style.zIndex = 10;
-        figCaption.style.zIndex = 20;
-      });
+        firstImg.style.zIndex = 10
+        figCaption.style.zIndex = 20
+      })
       card.addEventListener("mouseleave", (e) => {
         // secondImg.style.opacity = 1;
-        firstImg.style.zIndex = "";
-        figCaption.style.zIndex = "";
-      });
-    });
+        firstImg.style.zIndex = ""
+        figCaption.style.zIndex = ""
+      })
+    })
   }
 }
